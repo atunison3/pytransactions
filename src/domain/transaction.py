dada29d870a573bf682c3f46dd33305c50df5d87
atunison3 <atunison1@yahoo.com>
2024-11-22 12:33:00 -0600

    Create transaction.py

---
+++ src/domain/transaction.py
@@ -0,0 +1,14 @@
+class Transaction:
+    def __init__(
+        self, transaction_id: int | None, amount: float, date: str, description: str, 
+        category: str, notes: str
+    ):
+        self.transaction_id = transaction_id
+        self.amount = amount
+        self.date = date
+        self.description = description
+        self.category = category
+        self.notes = notes 
+
+    def __repr__(self):
+        return f"Transaction(id={self.transaction_id}, amount={self.amount}, date='{self.date}', description='{self.description}')"
